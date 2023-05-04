a57a7dc23c5c3a02d318f94a3b7368c3894ec6b0
Dilshika20 <kumarasinghedilshika20@gmail.com>
2023-05-04 16:01:07 +0530

    try

---
+++ app/src/main/java/com/example/myapplicationtute/models/validations/ValidationResult.kt
@@ -0,0 +1,9 @@
+package com.example.myapplicationtute.models.validations
+
+    sealed class ValidationResult{
+        data class Empty(val errorMessage:String): ValidationResult()
+        data class Invalid(val errorMessage: String): ValidationResult()
+        object Valid: ValidationResult()
+    }
+
+
